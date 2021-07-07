b78e92be3515519d2878a9367cf77b0182be59a9
PaweenK2326 <por2326@gmail.com>
2021-07-07 17:07:04 +0700

    one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test-one';
+export class AppComponent implements OnInit{
+
+  ngOnInit(){
+    this.checkNumber();
+  }
+
+  onChangeEvent(event): void{
+    let number = event.target.value;
+    if(number < 0){
+      event.target.value = 1;
+    } else if(!Number.isInteger(number)){
+      event.target.value = Math.round(number);
+    }
+    this.checkNumber();
+  }
+
+  checkNumber(): void{
+    const select = document.querySelector('select');
+    const input = document.querySelector('input');
+    const p = document.querySelector('p');
+
+    const value = parseInt(input.value);
+  
+    switch (select.value) {
+      case 'isPrime':
+        this.isPrime(value) ? p.innerHTML = 'true' : p.innerHTML = 'false';
+        break;
+      case 'isFibonacci':
+        this.isFibonacci(value) ? p.innerHTML = 'true' : p.innerHTML = 'false';
+        break;
+    }
+  }
+
+  isPrime(number: number): boolean{
+    for(let i = 2; i < number; i++)
+      if(number % i === 0) return false;
+    return number > 1;
+  }
+  isSquare(n: number): boolean{
+    return n > 0 && Math.sqrt(n) % 1 === 0;
+  }
+  isFibonacci(number: number): boolean{
+    if (this.isSquare(5*(number*number)-4) || this.isSquare(5*(number*number)+4)) {
+      return true;
+    } else { 
+      return false; 
+    }
+  }
 }
